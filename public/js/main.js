0d414a501107d14ba893831bd0b08a17ffbd3d54
angel <angeldesmogica@gmail.com>
2026-03-25 11:33:06 -0600

    estilo de la barra de navegación

--- public/js/main.js
+++ public/js/main.js
@@ -9,3 +9,28 @@ botonCerrarSesion.addEventListener("click", async () => {
         window.location.href = "/";
     }
 });
+
+//navbar
+const menuHamburger = document.getElementById("menu-hamburger");
+const sidebar = document.getElementById("sidebar");
+const sidebarOverlay = document.getElementById("sidebar-overlay");
+
+const abrirSidebar = () => {
+    sidebar.classList.add("sidebar-abierto");
+    sidebarOverlay.classList.add("sidebar-overlay-activo");
+}
+
+const cerrarSidebar = () => {
+    sidebar.classList.remove("sidebar-abierto");
+    sidebarOverlay.classList.remove("sidebar-overlay-activo");
+}
+
+menuHamburger.addEventListener("click", abrirSidebar);
+sidebarOverlay.addEventListener("click", cerrarSidebar);
+
+const paginaActual = window.location.pathname.split("/").pop();
+document.querySelectorAll(".menu-links a, .sidebar-nav a").forEach(link => {
+    if(link.getAttribute("href")===paginaActual){
+        link.classList.add("menu-link-activo");
+    }
+});
